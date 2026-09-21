2aabd02077aae6081b89e62a6690f2ee046b52be
Atharva Khare <atharvakhare624@gmail.com>
2026-09-21 21:55:53 +0530

    completed the search and vectorstore setting.

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from src.document_loader import document_loader
-from src.vectorstore import Faissvectorstore
+import os
+from dotenv import load_dotenv
+from src.search import RAGSearch
 
 
+load_dotenv()
 
-if __name__=="__main__":
-    #docs = document_loader("data")
-    store = Faissvectorstore("faiss_store")
-    #store.build_document(docs)
-    store.load()
-    print(store.query("What are the RBI compliance requirements and mandate rules when using corporate credit cards for recurring vendor payments or SaaS billing?",top_k=3))
+def main():    
+
+    if not os.getenv("GROQ_API_KEY"):
+        print("GROQ_API_KEY is missing from your .env file!")
+        return
+
+    rag_engine = RAGSearch(
+        persist_dir="notebook/vectorstore/db_faiss", 
+        embedding_model="all-MiniLM-L6-v2",
+        llm_model="openai/gpt-oss-120b"
+    )
+
+    query = input("enter your question:")
+    print(f"\nUSER QUERY: {query}\n")
+    print("Generating answer with LLM")
+    
+    response = rag_engine.search_and_summarize(query, top_k=4)
+
+    print("\nRESPONSE")
+    print(response)
+
+if __name__ == "__main__":
+    main()
---
+++ src/search.py
@@ -0,0 +1,40 @@
+import os
+from dotenv import load_dotenv
+from src.vectorstore import Faissvectorstore
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+class RAGSearch:
+    def __init__(self, persist_dir = "faiss_store", embedding_model= "all-MiniLM-L6-v2", llm_model="openai/gpt-oss-120b"):
+        self.vectorstore = Faissvectorstore(persist_dir, embedding_model)
+        
+
+        faiss_path = os.path.join(persist_dir, "faiss.index")
+        meta_path = os.path.join(persist_dir, "metadata.pkl")
+        if not (os.path.exists(faiss_path) and os.path.exists(meta_path)):
+            from src.document_loader import document_loader
+            docs = document_loader("data")
+            self.vectorstore.build_document(docs)
+        else:
+            self.vectorstore.load()
+            
+        self.llm = ChatGroq(model_name=llm_model)
+        print(f"Groq LLM initialized: {llm_model}")
+
+    def search_and_summarize(self, query, top_k= 5):
+        results = self.vectorstore.query(query, top_k=top_k)
+        texts = [r["metadata"].get("text", "") for r in results if r["metadata"]]
+        context = "\n\n".join(texts)
+        if not context:
+            return "No relevant documents found."
+        
+        prompt = f"""You are an expert B2B financial compliance assistant.
+Answer the question using strictly the context provided below. If you cannot answer the question using the context, state "I do not have enough information to answer that.": '{query}'
+
+Context:
+{context}
+
+Summary:"""
+        response = self.llm.invoke([prompt])
+        return response.content
